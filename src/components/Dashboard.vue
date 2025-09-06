0262696245eb3ce2d984a31f2ca8bf50da134cee
Darchi <darchi.jsav@gmail.com>
2025-09-05 19:46:55 -0500

    Subida del codigo inicial del login

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="dashboard">
+        <h1>Bienvenido al Dashboard</h1>
+        <button @click="cerrarSesion">Cerrar sesión</button>
+    </div>
+</template>
+
+<script setup>
+import { ref, reactive, onMounted, defineProps, defineEmits } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const props = defineProps({
+    visible: {
+        type: Boolean,
+        required: true
+    }
+})
+
+function cerrarSesion() {
+    localStorage.removeItem('sesion-activa');
+    router.push('/');
+}
+</script>
+
+<style scoped>
+.dashboard {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background: linear-gradient(135deg, #031A38FF, #9669FDFF);
+    color: white;
+    font-family: 'Luckiest Guy', cursive;
+}
+
+button {
+    background: white;
+    color: black;
+    border: none;
+    padding: 1rem 2rem;
+    margin-top: 1rem;
+    cursor: pointer;
+    border-radius: 8px;
+    font-size: 1.2rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
+// import {createPinia} from 'pinia'
+// import './style.css'
+import router from "./routes/routes.js"
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// const pinia = createPinia()
+const app = createApp(App)
+
+app.use(router)
+// app.use(pinia)
+app.mount('#app')
